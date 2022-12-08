b587bfaba5a3aeac5320a01e19f0d4efa818d540
Nikita Chauhan <nikitachauhan64@gmail.com>
2022-12-08 18:31:55 +0530

    Checked with EurekaServerApplication

--- BEJ_C2_S3_Rest_Api_MongoDB_MC_1/src/main/java/com/niit/BEJ_C2_S3_Rest_Api_MongoDB_MC_1/BejC2S3RestApiMongoDbMc1Application.java
+++ BEJ_C2_S3_Rest_Api_MongoDB_MC_1/src/main/java/com/niit/BEJ_C2_S3_Rest_Api_MongoDB_MC_1/BejC2S3RestApiMongoDbMc1Application.java
@@ -2,8 +2,10 @@ package com.niit.BEJ_C2_S3_Rest_Api_MongoDB_MC_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BejC2S3RestApiMongoDbMc1Application {
 
     public static void main(String[] args) {
